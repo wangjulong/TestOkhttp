991f6565e5a0c4eec547b96144f885fe1eff15b4
wangjulong <496349619@qq.com>
2016-09-05 11:38:52 +0800

    SugarOrm

--- app/src/main/java/com/example/administrator/testokhttp/MainActivity.java
+++ app/src/main/java/com/example/administrator/testokhttp/MainActivity.java
@@ -7,6 +7,8 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 
+import com.orm.SugarRecord;
+
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,13 +23,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateKJH(View view) {
 
-        MyAsynctask task = new MyAsynctask();
 //        String strUrl = "http://chart.cp.360.cn/zst/ln11/?span=100";
 //        String strUrl = "http://www.52cp.cn/bull/index.php/Index/list_ln11";
 //        String strUrl = "http:////zs.cailele.com//ln11x5//baseTrend.php?t=100";
-        String strUrl = "http://trend.caipiao.163.com/ln11xuan5/?periodNumber=100";
 
-        task.execute(strUrl);
+        for (int i=0;i<10;i++) {
+            Kjh kjh = new Kjh("abc"+i,i,i*2,i*3,i*4,i*5);
+            kjh.save();
+        }
+        textView = (TextView) findViewById(R.id.textView);
+        String s = Kjh.findById(Kjh.class,3).title + Kjh.findById(Kjh.class,3).n3;
+        textView.setText(s);
+
+//        MyAsynctask task = new MyAsynctask();
+//        String strUrl = "http://trend.caipiao.163.com/ln11xuan5/?periodNumber=100";
+//        task.execute(strUrl);
     }
 
 
@@ -52,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
             //s 网页内容
 //            String sep = "<td class='tdbg_1'>";
 //            String[] str = s.split(sep);
-            textView = (TextView)findViewById(R.id.textView);
+            textView = (TextView) findViewById(R.id.textView);
             textView.setText(s);
             textView.setMovementMethod(ScrollingMovementMethod.getInstance());
 //            System.out.print(s);
